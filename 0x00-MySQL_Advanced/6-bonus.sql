eea4d176ea216936363439aba4793d10db5442d4
tewa92 <tewachewsaleigizer@gmail.com>
2024-08-08 07:18:06 -0700

    mysql

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,22 +1,38 @@
 DROP PROCEDURE IF EXISTS AddBonus;
-DELIMITER $$
-CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score FLOAT)
 
-BEGIN
-DECLARE project_count INT DEFAULT 0;
+DELIMITER $ $ CREATE PROCEDURE AddBonus (
+    user_id INT,
+    project_name VARCHAR(255),
+    score FLOAT
+) BEGIN DECLARE project_count INT DEFAULT 0;
+
 DECLARE project_id INT DEFAULT 0;
 
-SELECT COUNT(ID)
-INTO project_count
-FROM projects
-WHERE name = project_name;
+SELECT
+    COUNT(id) INTO project_count
+FROM
+    projects
+WHERE
+    name = project_name;
+
 IF project_count = 0 THEN
-INSERT INTO projects(name) VALUES(project_name);
+INSERT INTO
+    projects(name)
+VALUES
+(project_name);
+
 END IF;
-SELECT id
-INTO project_id
-FROM projects
-WHERE name = project_name;
-INSERT INTO corrections VALUES(user_id, project_id, score);
-END$$
-DELIMITER ;
+
+SELECT
+    id INTO project_id
+FROM
+    projects
+WHERE
+    name = project_name;
+
+INSERT INTO
+    corrections(user_id, project_id, score)
+VALUES
+    (user_id, project_id, score);
+
+END $ $ DELIMITER;
