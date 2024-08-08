97bff07f42fae9044ae8116b06467344b447b34d
tewa92 <tewachewsaleigizer@gmail.com>
2024-08-08 04:51:18 -0700

    country users

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- creates a table users with a unique id, email, country and name field.
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+email VARCHAR(255) NOT NULL UNIQUE,
+name VARCHAR(255),
+country ENUM('US', 'CO', 'TN')
+);
