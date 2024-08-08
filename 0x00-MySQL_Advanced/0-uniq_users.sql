7c59dbbe45f3d2b8a1e8e87d8bad705a11bc8fd7
tewa92 <tewachewsaleigizer@gmail.com>
2024-08-08 03:55:11 -0700

    uniq user

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,6 @@
 -- creates a table users with a unique id, email and name field.
 
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
 id int() no null auto_increment primary key,
 email string(255) not null unique,
