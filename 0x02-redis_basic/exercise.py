ed41efe446507bc288feb274ff4f911bebb80455
tewa92 <tewachewsaleigizer@gmail.com>
2024-08-14 14:54:21 -0700

    exercise

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+''' Exercise module '''
+
+
+from typing import Any, Callable, Union
+import redis
+import uuid
+
+class Cache:
+    def __init__(self) -> None:
+        ''' Initialize the cache '''
+        self._redis = redis.Redis()
+        self._redis.flushdb(True)
+        
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        ''' Store data in cache and return a key to retrieve it '''
+        data_key = str(uuid.uuid4())
+        self_redis = redis.set(data_key, data)
+        return data_key
