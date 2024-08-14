2b9393ff0fa68c7640e2fa7cefb8d64a2c4b5dda
tewa92 <tewachewsaleigizer@gmail.com>
2024-08-14 14:57:07 -0700

    exercise

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,12 +6,13 @@ from typing import Any, Callable, Union
 import redis
 import uuid
 
+
 class Cache:
     def __init__(self) -> None:
         ''' Initialize the cache '''
         self._redis = redis.Redis()
         self._redis.flushdb(True)
-        
+ 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         ''' Store data in cache and return a key to retrieve it '''
         data_key = str(uuid.uuid4())
